5473360688e5aa7a4732a84c408555b4b638adc1
mohamedsaad81973 <mohamedsaad81973@gmail.com>
2024-03-16 23:06:37 +0000

    fix char error

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -8,14 +8,16 @@ export default function HeroSection() {
     <div className="min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between animate-move-up">
       <div className="space-y-10 text-center lg:text-left">
         <h1 className="text-4xl lg:text-7xl font-bold">
-          {`Nice to meet you! 👋 `}<br />{" "}
+          {"Nice to meet you! 👋 "}
+          <br />{" "}
           <span className="underline underline-offset-8 decoration-green-500">
-            {`I'm Abdelrhman.`}
+            {"I'm Abdelrhman."}
           </span>
         </h1>
         <p className="md:w-96 text-lg text-gray-300">
-          {`Based in Egypt, I'm a Fullstack developer passionate about building
-          modern web applications that users love.`}
+          {
+            "Based in Egypt, I'm a Fullstack developer passionate about building modern web applications that users love."
+          }
         </p>
         <Link
           className="inline-block group"
@@ -37,10 +39,9 @@ export default function HeroSection() {
           <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
         </div>
         <div className="absolute bottom-5 sm:bottom-14 left-0 sm:-left-10">
-
-        <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
-          <p>{`💼 Available for work`}</p>
-        </MovingBorderBtn>
+          <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
+            <p>{"💼 Available for work"}</p>
+          </MovingBorderBtn>
         </div>
       </div>
     </div>
