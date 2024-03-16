2c5ad6fab3355dedc74ade2dde073325a56e860b
mohamedsaad81973 <mohamedsaad81973@gmail.com>
2024-03-16 23:00:32 +0000

    fix title error

--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -10,7 +10,7 @@ export default function Title({
   return (
     <div className={className}>
       <h2 className="text-3xl font-bold group-hover:text-green-400 transition-all">
-        {text}
+        {`${text}`}
       </h2>
       <div className="w-40 h-2 bg-green-500 rounded-full" />
       <div className="w-40 h-2 bg-indigo-500 rounded-full translate-x-2" />
